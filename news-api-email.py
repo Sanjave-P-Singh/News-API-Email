ae0daac9dcb699ab9d1561809c84f4cac4a98fa4
sanjave <ssingh025@citymail.cuny.edu>
2023-10-03 22:47:58 -0400

    Day 28: emails you the news

--- news-api-email.py
+++ news-api-email.py
@@ -1,9 +1,14 @@
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 api_key = "83eb3850927e41d9b49f340c8f21287b"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-09-04&sortBy=publishedAt&apiKey=" \
-      "83eb3850927e41d9b49f340c8f21287b"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=83eb3850927e41d9b49f340c8f21287b&" \
+      "language=en"
 
 # Make a request
 request = requests.get(url)
@@ -12,6 +17,12 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+body = "Subject: Today's news " + "\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
